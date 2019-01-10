45d89ac8870a2c1b2faba03e6c6292ecad6d9ca9
juliely8619 <july8619@yahoo.com>
2019-01-09 18:01:23 -0800

    tested the xunit

--- lab2-unittesting/Program.cs
+++ lab2-unittesting/Program.cs
@@ -2,11 +2,18 @@
 
 namespace lab2_unittesting
 {
-    class Program
+    public class Program // added public so can call it in the test environment
     {
+        public static double balance = 5000.00;
         static void Main(string[] args)
         {
-            
+            //this is where the switch will go
+            ReturnsBalance(balance);
+        }
+
+        public static double ReturnsBalance(double balance)
+        {
+        return balance;
         }
 
     }
